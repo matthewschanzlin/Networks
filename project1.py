89e68577cf642096c57b8916142bec0a6a1ef73a
matthew schanzlin <matthewschanzlin@gmail.com>
2020-09-12 15:19:29 -0400

    ssl is working

--- project1.py
+++ project1.py
@@ -1,7 +1,8 @@
 import socket
 import sys
+import ssl
 
-PORT = 27993
+PORT = 0
 HOSTNAME = ''
 NUID = 0
 args = sys.argv
@@ -22,16 +23,20 @@ for i in range(1, len(args)):
         need_hostname = False
         HOSTNAME = arg
     else:
-        NUID = int(arg)
+        NUID = arg
 
 print('PORT ', PORT)
 print('use_ssl ', use_ssl)
 print('HOSTNAME ', HOSTNAME)
 print('NUID ', NUID)
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOSTNAME, PORT))
-s.send('cs3700fall2020 HELLO 001699953\n')
+if use_ssl:
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    s = context.wrap_socket(s, server_side=False, server_hostname=HOSTNAME)
+print('SOCKET ', s)
+s.send(bytes('cs3700fall2020 HELLO {}\n'.format(NUID)))
+
 bits = 1024
 finding = True
 flag = ''
@@ -42,24 +47,22 @@ while finding:
     data = ''
     while more_data:
         this_data = s.recv(bits)
-        if len(this_data) == 0:
+        data += this_data
+        if '\n' in this_data:
             more_data = False
-        else:
-            data += this_data
 
     data_pieces = data.split(' ')
-    print('LEN ', len(data_pieces))
     message = data_pieces[1]
+
     if message == 'FIND':
         symbol = data_pieces[2]
         symbols = data_pieces[3]
         count = symbols.count(symbol)
         response = 'cs3700fall2020 COUNT {}\n'.format(str(count))
-        print('response ', response)
-        s.send(response)
+        s.send(bytes(response))
+
     elif message == 'BYE':
         finding = False
         flag = data_pieces[2]
-        print('FLAG ', flag)
-    print('LOOP BACK')
+
 print('FLAG ', flag)
