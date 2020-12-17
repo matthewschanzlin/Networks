32b8a460bf650ff360a450dcd2d2382ce9956161
matthew schanzlin <matthewschanzlin@gmail.com>
2020-12-16 21:59:22 -0500

    more utils

--- project3/routerUtils.py
+++ project3/routerUtils.py
@@ -70,3 +70,82 @@ class RouterUtils:
             ports[key] = routes.get(key)
 
         return ports
+
+    def sameAttributes(msg1, msg2):
+        return ((msg1['src'] == msg2['src']) and
+                (msg1['msg']['localpref'] == msg2['msg']['localpref']) and
+                (msg1['msg']['selfOrigin'] == msg2['msg']['selfOrigin']) and
+                (msg1['msg']['ASPath'] == msg2['msg']['ASPath']) and
+                (msg1['msg']['origin'] == msg2['msg']['origin']))
+
+    def canCoalesceKey(key1, key2):
+        ip1, cidr1 = key1.split("/")
+        ip2, cidr2 = key2.split("/")
+        if (cidr1 == cidr2):
+          bin1 = list(''.join(format(int(x), '08b') for x in ip1.split(".")))
+          bin2 = list(''.join(format(int(x), '08b') for x in ip2.split(".")))
+          idx = int(cidr1) - 1
+
+          if ((bin1[idx] == '1') and (bin2[idx] == '0')):
+            return True
+          if ((bin1[idx] == '0') and (bin2[idx] == '1')):
+            return True
+        return False
+
+
+    def getCoalesce(routesAndUpdates):
+        for key1, value1 in routesAndUpdates.items():
+          for key2, value2 in routesAndUpdates.items():
+            if (canCoalesceKey(key1, key2)):
+              for msg1 in value1:
+                for msg2 in value2:
+                  if sameAttributes(msg1, msg2):
+                    return [key1, key2]
+
+        return None
+
+    def handleCoalesce(key1, key2, routes, routesAndUpdates):
+        ip, cidr = key1.split("/")
+        binary = list(''.join(format(int(x), '08b') for x in ip.split(".")))
+        binary[int(cidr) - 1] = '0'
+        newipbinary = ''.join(binary)
+        groupsOfEight = [newipbinary[i: i + 8] for i in range(0, len(newipbinary), 8)]
+        for i in range(len(groupsOfEight)):
+          groupsOfEight[i] = str(int(groupsOfEight[i], 2))
+
+        newIp = '.'.join(groupsOfEight)
+        newCidr = str(int(cidr) - 1)
+
+        newKey = newIp + '/' + newCidr
+
+        newNetmask = str(IPv4Network(newKey).netmask)
+        updates1 = routesAndUpdates[key1]
+        updates2 = routesAndUpdates[key2]
+
+        for i in range(len(updates1)):
+          for j in range(len(updates2)):
+            if sameAttributes(updates1[i], updates2[j]):
+              newMsg = updates1[i]
+              newMsg['msg']['network'] = newIp
+              newMsg['msg']['netmask'] = newNetmask
+              self.placeUpdateInOrder(newKey, newMsg)
+              updates1.pop(i)
+              updates2.pop(j)
+              routesAndUpdates[key1] = updates1
+              routesAndUpdates[key2] = updates2
+              routes[newKey] = routesAndUpdates[newKey][0]["src"]
+
+              if len(routesAndUpdates[key1]) == 0:
+                del routes[key1]
+                del routesAndUpdates[key1]
+              else:
+                routes[key1] = routesAndUpdates[key1][0]["src"]
+
+              if len(routesAndUpdates[key2]) == 0:
+                del routes[key2]
+                del routesAndUpdates[key2]
+              else:
+                routes[key2] = routesAndUpdates[key2][0]["src"]
+
+              return True
+        return False
